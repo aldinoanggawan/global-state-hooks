5d09d5ff881dd0b883f70acabb69804ca5ef59e7
Aldino Dwi Anggawan <aldinoanggawan@gmail.com>
2020-03-22 03:28:31 +0800

    Added GlobalStateProvider in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import { GlobalStateProvider } from './hooks/useGlobalState'
 
 import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
-    <>
-      <HomePage/>
-    </>
+    <GlobalStateProvider>
+      <HomePage />
+    </GlobalStateProvider>
   )
 }
 

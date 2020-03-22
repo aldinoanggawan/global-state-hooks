add3c407c8f6aec6ea2929082570ba1396314b29
Aldino Dwi Anggawan <aldinoanggawan@gmail.com>
2020-03-22 23:38:34 +0800

    Added a form to change the global state

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import useGlobalState from '../hooks/useGlobalState'
 
 const HomePage = () => {
+    const globalState = useGlobalState()
+    const currentId = globalState.id
+
+    const [currentState, setCurrentState] = useState({
+        name: '',
+        age: '',
+        occupation: '',
+    })
+
+    const handleChange = e => {
+        const { name, value } = e.target
+
+        setCurrentState({
+            ...currentState,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        globalState.setId(currentState)
+    }
+
     return(
         <>
-            <h1>Hello World!</h1>
+            <h2>Name: {currentId.name}</h2>
+            <h2>Age: {currentId.age}</h2>
+            <h2>Occupation: {currentId.occupation}</h2>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleChange} name="name" type="text" placeholder="Enter name here" />
+                <input onChange={handleChange} name="age" type="text" placeholder="Enter age here" />
+                <input onChange={handleChange} name="occupation" type="text" placeholder="Enter occupation here" />
+                <input type="submit" value="Change global state" />
+            </form>
         </>
     )
 }
